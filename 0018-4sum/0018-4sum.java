14c01825569d9d26c74b7dffcc9a5c58a8b4da67
Vicky Kumar <129484111+Vickykumarverma1@users.noreply.github.com>
2026-06-30 19:31:09 +0530

    Time: 20 ms (64.82%), Space: 45.8 MB (62.28%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,48 +1,37 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-       for(int i = 0; i<nums.length - 3; i++)
-       {
-        if(i>0 && nums[i]==nums[i-1])
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i = 0; i < nums.length-3; i++) //why n<-3 because we have to find quadruplets
         {
-            continue;
-        }
-            for(int j = i+1; j<nums.length-2; j++)
+            if(i > 0 && nums[i] == nums[i-1]) continue; // for skipping the duplicates
+            for(int j = i+1; j < nums.length-2; j++)
             {
-                if(j>i+1 && nums[j]== nums[j-1]) continue;
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
                 int low = j+1;
                 int high = nums.length-1;
                 while(low<high)
                 {
-                    long sum = (long) nums[i]+nums[j] + nums[low]+ nums[high];
+                    long sum = (long) nums[i] + nums[j] + nums[low] + nums[high];
                     if(sum == target)
                     {
-                        res.add(Arrays.asList(nums[i],nums[j],nums[low],nums[high]));
+                        result.add(Arrays.asList(nums[i], nums[j], nums[low], nums[high]));
                         low++;
                         high--;
-                        while(low<high && nums[low]==nums[low-1])
-                        {
-                            low++;
-                        }
-                        while(low<high && nums[high]==nums[high+1])
-                        {
-                            high--;
-                        }
+                        while(low < high && nums[low] == nums[low-1]) low++;
+                        while(low < high && nums[high] == nums[high+1]) high--;
                     }
-                    else if(sum>target)
+                    else if(sum > target)
                     {
                         high--;
                     }
-                    else
+                    else 
                     {
                         low++;
                     }
                 }
             }
-
-       }
-       return res;
-
+        }
+        return result;
     }
 }
