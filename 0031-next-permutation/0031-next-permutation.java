b72056fc25daa042455b1406d376d0ccbaba4c3f
Vicky Kumar <129484111+vickyverrma@users.noreply.github.com>
2026-08-13 10:37:26 +0530

    Time: 0 ms (100%), Space: 44.4 MB (85.39%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,47 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int index = -1;
+        int n = nums.length;
+        for(int i = n-2; i>=0; i--)
+        {
+            if(nums[i]<nums[i+1])
+            {
+                index = i;
+                break;
+            }
+        }
+        if(index == -1)
+        {
+            reverse(nums ,0,n-1);
+            return;
+        }
+        for(int i = n-1; i>=index; i--)
+        {
+            if(nums[i]>nums[index])
+            {
+                swap(nums,i,index);
+                break;
+            }
+        }
+        reverse(nums, index + 1, n - 1);
+        return;
+    }
+    static void swap(int[] nums, int start , int end)
+    {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
+
+    static  void reverse(int[] nums, int start, int end)
+    {
+        while(start<=end)
+        {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end --;
+        }
+    }
+}
