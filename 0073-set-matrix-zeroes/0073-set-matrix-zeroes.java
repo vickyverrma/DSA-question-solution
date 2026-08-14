47b2e8fc796baa413dd951ebe0cdf70f4e5d8c38
Vicky Kumar <129484111+vickyverrma@users.noreply.github.com>
2026-08-14 23:23:37 +0530

    Time: 1 ms (97.47%), Space: 47.9 MB (5.95%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,50 +1,29 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        // Your code goes here
         int m = matrix.length; //rows
         int n = matrix[0].length; //column
-        boolean flag = true;
+        boolean[] row = new boolean[m];
+        boolean[] column = new boolean[n];
         for(int i = 0; i<m; i++)
         {
             for(int j = 0; j<n; j++)
             {
-                if(matrix[i][j]==0)
+               if(matrix[i][j]==0)
+               {
+                row[i]= true;
+                column[j]= true;
+               } 
+            }
+        }
+        for(int i = 0; i<m; i++ )
+        {
+            for(int j = 0; j<n; j++)
+            {
+                if(row[i]==true || column[j]==true)
                 {
-                    for(int col = 0; col<n; col++)
-                    {
-                        if(matrix[i][col]!=0)
-                        {
-                            matrix[i][col]=Integer.MIN_VALUE+2;
-                            flag = false;
-                        }
-                    }
-                    for(int row = 0; row<m; row++)
-                    {
-                        if(matrix[row][j]!=0)
-                        {
-                            matrix[row][j]=Integer.MIN_VALUE+2;
-                            flag = false;
-                        }
-                    }
+                    matrix[i][j]=0;
                 }
             }
         }
-        if(flag == false)
-        {
-            for(int i = 0; i<m; i++)
-            {
-                for(int j = 0; j<n; j++)
-                    {
-                        if(matrix[i][j]==Integer.MIN_VALUE+2)
-                            {
-                                matrix[i][j]=0;
-                             }
-                    }
-            }
-        }
-        else
-        return;
-        
-        
     }
 }
