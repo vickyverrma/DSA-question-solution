eafa33cdbb0924664e2a037b2ed2b6f2089d03cf
Vicky Kumar <129484111+vickyverrma@users.noreply.github.com>
2026-09-01 18:58:50 +0530

    Time: 5 ms (62.67%), Space: 44.5 MB (66.5%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,26 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        int[] freqs = new int[26];
-        int[] freqt = new int[26];
-        for (char c : s.toCharArray()) {
-            freqs[c - 'a']++;
-        }
-        for (char c : t.toCharArray()) {
-            freqt[c- 'a']++;
-        }
-        for(int i = 0; i<26; i++)
-        {
-            if(freqs[i]==freqt[i])
-            {
-                continue;
-            }
-            else
+        // Optimization 1: Early exit if lengths don't match
+        if (s.length() != t.length()) {
             return false;
         }
+        
+        // Optimization 2: Single frequency array
+        int[] freq = new int[26];
+        
+        // You can do both in a single loop since they are the same length
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
+        }
+        
+        // Check if any frequency is non-zero
+        for (int count : freq) {
+            if (count != 0) {
+                return false;
+            }
+        }
+        
         return true;
-
     }
 }
